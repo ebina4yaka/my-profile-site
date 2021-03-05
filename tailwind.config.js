0f2a860067e7cd7af5ed713a5e1c821b7ba8f7e4
ebina4yaka <4yaka3bina@gmail.com>
2021-03-05 14:56:12 +0900

    Update UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,28 +2,7 @@ module.exports = {
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        pink: {
-          light: '#FCE7F3',
-          dark: '#831843',
-        },
-        green: {
-          light: '#D1FCCF',
-          dark: '#0A8211',
-
-        },
-        blue: {
-          light: '#DCE8FC',
-          dark: '#113382',
-
-        },
-        yellow: {
-          light: '#FCEDCF',
-          dark: '#826624',
-        },
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
